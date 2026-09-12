8c8eab453a110dd0a25fc8c7ed0ee89cd5a2b351
Caio Curcio <caio.curcio@outlook.com>
2026-09-11 22:42:20 -0300

    test(security): vulnerabilidade corrigida

--- services/flag/app.py
+++ services/flag/app.py
@@ -45,7 +45,7 @@ def init_db_command():
 @app.route("/debug/echo")
 def debug_echo():
     msg = request.args.get("msg", "")
-    subprocess.call(f"echo {msg}", subprocess.call(["echo", msg]))  # inseguro de proposito p/ demo do Bandit
+    subprocess.call(["echo", msg])
     return {"echoed": msg}
 
 
